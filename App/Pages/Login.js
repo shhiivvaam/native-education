71a563335ae7ef023dc10c021f90e6c2270f5e67
Shivam Kumar <life.shivam2394@gmail.com>
2024-07-07 20:56:10 +0530

    adding auth

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Login() {
     return (
         <View>
-            <Text>Login</Text>
-        </View>
+            <Image source={require("../Assets/Images/login.png")} />
+            <View style={styles.container}>
+                <Text style={styles.welcomeText}>
+                    Help us help you!!
+                </Text>
+            </View>
+        </View >
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 40,
+        marginTop: -25,
+        backgroundColor: '#fff',
+        borderTopRightRadius: 30,
+        borderTopStartRadius: 30,
+    },
+    welcomeText: {
+        fontSize: 35,
+        textAlign: "center",
+        fontWeight: "bold",
+    }
+});
